307c9b6ee79e724dd360776718e4d14f76116816
overdrivenpotato <marko.mijalkovic97@gmail.com>
2013-11-17 00:26:03 -0500

    Fixed pipe location

--- game.js
+++ game.js
@@ -13,7 +13,9 @@ var Q = Quintus()
     .include("Sprites, Scenes, Input, 2D, Touch, UI")
     .setup({
         width: 960,
-        height: 640
+        height: 640,
+        downsampleWidth: 1,
+        downsampleHeight: 1
 //        maximize: true
     }).controls().touch();
 //load assets
@@ -33,6 +35,9 @@ Q.Sprite.extend("Player",{
         if(Q.inputs['right']  && this.p.direction == 'left') {
             this.p.flip = false;
         }
+//        if(this.vy > 0)
+//            this.vx = this.vx * 2;
+//        console.log(this.vy);
         if(this.p.x < 0)
             this.p.x = 0;
         if(this.p.x > stageMaxX)
@@ -47,7 +52,7 @@ Q.Sprite.extend("Player",{
     }
 });
 
-Q.Sprite.extend("Enmy",{
+Q.Sprite.extend("Enemy",{
     init: function(p) {
         this._super(p, { asset: 'enemy.png', vx: 100 });
         this.add('2d, aiBounce');
@@ -72,8 +77,9 @@ Q.Sprite.extend("Enmy",{
 
 Q.Sprite.extend("Pipe",{
     init: function(p){
-        this._super(p, { asset: 'pipe.png', x: 100, y: 200}) ;
-        this.on("bump.top",function(collision) {
+        this._super(p, { asset: 'pipe.png', x: 3045, y: 280}) ;
+        this.add('2d');
+        this.on("bump.top", function(collision) {
             console.log(Q.inputs['down']);
             console.log(collision.obj.isA("Player"));
             if(collision.obj.isA("Player") && Q.inputs['down']) {
@@ -89,7 +95,7 @@ Q.scene("level2", function(stage) {
     stage.insert(background);
     stage.collisionLayer(new Q.TileLayer({ dataAsset: level, layerIndex:1,  sheet: 'tiles', tileW: 70, tileH: 70 }));
     var player = stage.insert(new Q.Player());
-    var enemy = stage.insert(new Q.Enmy({ x: 700, y: 0 }));
+    var enemy = stage.insert(new Q.Enemy({ x: 700, y: 0 }));
     var pipe = stage.insert(new Q.Pipe());
     stageMaxX = background.p.w;
     stageMaxY = background.p.h;
