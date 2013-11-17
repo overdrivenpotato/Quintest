eb4eaf7f503d2140d41194f92ffc9b4b34ae6ea1
overdrivenpotato <marko.mijalkovic97@gmail.com>
2013-11-16 20:16:08 -0500

    Added Enemy

--- game.js
+++ game.js
@@ -16,7 +16,7 @@ var Q = Quintus()
 
 Q.Sprite.extend("Player",{
     init: function(p) {
-        this._super(p, { asset: "player.png", x: 110, y: 50, jumpSpeed: -680});
+        this._super(p, { asset: "player.png", x: 110, y: 50, jumpSpeed: -580});
         this.add('2d, platformerControls');
     },
     step: function(dt) {
@@ -26,26 +26,44 @@ Q.Sprite.extend("Player",{
         if(Q.inputs['right']  && this.p.direction == 'left') {
             this.p.flip = false;
         }
-
+        if(this.p.x < this.stage.minX)
+            this.p.x = this.stage.minX;
     }
 });
 
-Q.Sprite.extend("Pipe", {
-    init: function(p)
-    {
+Q.Sprite.extend("Enemy",{
+    init: function(p) {
+        this._super(p, { asset: 'enemy.png', vx: 100 });
+        this.add('2d, aiBounce');
 
+        this.on("bump.left,bump.right,bump.bottom",function(collision) {
+            if(collision.obj.isA("Player")) {
+                Q.stageScene("endGame",1, { label: "You Died" });
+                collision.obj.destroy();
+                Q.clearStages();
+                Q.stageScene("level2");
+            }
+        });
+
+        this.on("bump.top",function(collision) {
+            if(collision.obj.isA("Player")) {
+                this.destroy();
+                collision.obj.p.vy = -300;
+            }
+        });
     }
-})
+});
 
-Q.scene("level1",function(stage) {
-    var background = new Q.TileLayer({ dataAsset: 'level1.tmx', layerIndex: 0, sheet: 'tiles', tileW: 70, tileH: 70, type: Q.SPRITE_NONE });
+Q.scene("level2",function(stage) {
+    var background = new Q.TileLayer({ dataAsset: 'level3.tmx', layerIndex: 0, sheet: 'tiles', tileW: 70, tileH: 70, type: Q.SPRITE_NONE });
     stage.insert(background);
-    stage.collisionLayer(new Q.TileLayer({ dataAsset: 'level1.tmx', layerIndex:1,  sheet: 'tiles', tileW: 70, tileH: 70 }));
+    stage.collisionLayer(new Q.TileLayer({ dataAsset: 'level3.tmx', layerIndex:1,  sheet: 'tiles', tileW: 70, tileH: 70 }));
     var player = stage.insert(new Q.Player());
+    var enemy = stage.insert(new Q.Enemy({ x: 700, y: 0 }));
     stage.add("viewport").follow(player,{x: true, y: true},{minX: 0, maxX: background.p.w, minY: 0, maxY: background.p.h});
 });
 
-Q.load("tiles_map.png, player.png, level1.tmx", function() {
+Q.load("tiles_map.png, player.png, enemy.png, level3.tmx", function() {
     Q.sheet("tiles","tiles_map.png", { tilew: 70, tileh: 70});
-    Q.stageScene("level1");
+    Q.stageScene("level2");
 });
